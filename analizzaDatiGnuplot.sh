9ac8f953740f11a514677e86c14e677c58f22dab
Fisica2014-FF <directoryh22@gmail.com>
2014-02-25 23:31:54 +0100

    Grafici

---
+++ analizzaDatiGnuplot.sh
@@ -0,0 +1,1 @@
+./Gnuplot/*.gnuplotscript | gnuplot -p
--- genera_script_velocita.sh
+++ genera_script_velocita.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 for template in ./Gnuplot/TemplateScript/*.templatescript
 do
-	for datafile in ./Risultati/*/*
+	for datafile in ./Risultati/MetaDati/*/velocita*
 	do
 		# Togli templatescriptdal nome del file
 		nometemplate=$(echo "$template" | grep --perl-regexp --only-matching '(.*)\.(.*)' | perl -pe 's/(.*\/)(.+)\.(.+)/\2/g')
 		nomedata=$(echo "$datafile" | grep --perl-regexp --only-matching '(.*)\.(.*)' | perl -pe 's/(.*\/)(.+)\.(.+)/\2/g')
 		nomescript=$(echo "$(echo $nometemplate)_$(echo $nomedata).gnuplotscript")
+		tipodati=$(echo "$nomedata" | perl -pe 's/velocita_(.*)/\1/g' )
 		
 		echo "Nome template: $nometemplate"
 		echo "Nome data: $nomedata"
 		echo "Dati: $datafile"
 		echo "Nome Script: $nomescript"
+		echo "Nome Script: $tipodati"
 		
 		#cp --force $template ./Gnuplot/${nometemplate}_${nomedata}.gnuplotscript
 		#cp --force $template $nomescript
@@ -20,17 +22,18 @@ do
 		
 		#Togli le prime quattro righe del file, che sono i commenti del template
 		#tail -$(($(wc -l "/dev/fd/0" | grep -Po "(\d)*\s")-4))
-		cat $template | \
-		perl -pe "s{__PATHDATI__}{./Risultati/Analisi_Dati/$nomedata}g" | \
+		strnomelog="s{__NOMELOGFILE__}{../Risultati/Analisi_Dati/ris_$tipodati}g"
+		strfiletempo="s{__PATHFILETEMPO__}{../Risultati/Analisi_Dati/arrayTempi_$nomedata\.txt}g"
+		strvelocita="s{__PATHFILEVELOCITA__}{../Risultati/Analisi_Dati/velocita_$nomedata\.txt}g"
 		
-		perl -pe "s/__MAX__/$Massimo/g" | \
-		perl -pe "s/__MIN__/$Minimo/g" | \
-		perl -pe "s/__MEDIA__/$Media/g" | \
-		perl -pe "s{__PATHDATI__}{$datafile}g" | \
-		perl -pe "s/__SIGMAP__/$Dev_Standard_P/g" | \
-		perl -pe "s/__NUMDATI__/$NumeroDati/g" | \
-		perl -pe "s/__NOMEDATI__/$nomedata/g" \
-		>./Gnuplot/$nomescript
+		echo "$strnomelog"
+		echo "$strfiletempo"
+		
+		cat $template | \
+		perl -pe $strnomelog | \
+		perl -pe $strfiletempo | \
+		perl -pe $strvelocita | \
+		tee ./Gnuplot/$nomescript
 	done
 done
 
